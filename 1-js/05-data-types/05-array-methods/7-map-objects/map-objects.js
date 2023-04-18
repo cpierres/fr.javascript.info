ebad9b79fe44b908e77738c09fe55c72a4c6ce62
cpierres <cpierres@hotmail.com>
2023-04-18 11:24:11 +0200

    A partir d'un tableau d'objets, créer un autre tableau d'objets en transformant chaque élément (map)

---
+++ 1-js/05-data-types/05-array-methods/7-map-objects/map-objects.js
@@ -0,0 +1,24 @@
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [john, pete, mary];
+
+//NE PAS OUBLIER PARENTHESES APRES => CAR IL Y A UN BODY ;
+//s'il n'y avait qu'une seule ligne parenthèses non souhaitables
+let usersMapped = users.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id
+}));
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+
+console.log(usersMapped[0].fullName) // John Smith
+console.log(usersMapped[0].id) // 1
+
