a823a4b5225d0be045775eee1b170f70906aa791
cpierres <cpierres@hotmail.com>
2023-04-19 14:06:56 +0200

    random-min-max : nombre aléatoire avec décimales entre 2 valeurs

---
+++ 1-js/05-data-types/02-number/8-random-min-max/random-min-max.js
@@ -0,0 +1,8 @@
+//nombre aléatoire avec décimales entre 2 valeurs
+function random(min, max)  {
+    return min + Math.random() * (max - min);
+}
+
+console.log(random(1, 5));
+console.log(random(1, 5));
+console.log(random(1, 5)); 
